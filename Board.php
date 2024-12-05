4b005a58d5ed6b252fc819c52f527f1263a58aa4
호정 <w2314@e-mirim.hs.kr>
2024-12-05 23:24:24 +0900

    feat : 사용자가 입력한 값 받아오기

--- Board.php
+++ Board.php
@@ -1,4 +1,14 @@
 <?php
+// 사용자가 입력한 값 받아오기
+$name = $_POST['name'];
+$title = $_POST['title'];
+$grade = $_POST['grade'];
+$phone = $_POST['phone'];
+$detail = $_POST['detail'];
+$file = $_FILES['file'];
+
+// DB 연결
 include('./conn.php');
+
 
 ?>
