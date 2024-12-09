6a4c79bf9d60026cea8dc5c56b7c99bf042cd38e
호정 <w2314@e-mirim.hs.kr>
2024-12-10 08:53:45 +0900

    🧙🏻‍♂️ table 다시 만들고 conn.php 수정

--- conn.php
+++ conn.php
@@ -1,8 +1,10 @@
 <?php
-$host = 'localhost';
-$id = 'root';
-$pass = '111111';
-$db = 'testdb';
+$host = 'localhost';    // DB 서버 주소
+$id = 'root';           // 사용자 이름
+$pass = '111111';       // 비밀번호
+$db = 'testdb';         // 데이터베이스 이름
+
+// DB 연결
 $conn = mysqli_connect($host, $id, $pass, $db);
 
 ?>
